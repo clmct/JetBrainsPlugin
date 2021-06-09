c79d185109a42d20369d95296d54ca1fad42685b
GitHub Action <action@github.com>
2021-06-09 11:35:32 +0000

    Template cleanup

--- src/main/kotlin/com/github/clmct/jetbrainsplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/clmct/jetbrainsplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.clmct.jetbrainsplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.clmct.jetbrainsplugin.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/clmct/jetbrainsplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/clmct/jetbrainsplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.clmct.jetbrainsplugin.services
 
+import com.github.clmct.jetbrainsplugin.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
